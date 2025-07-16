47d8bbd91a8dfdf3efc3b6a03ca8eb212c17b01e
Didin PG <didingangadharpg@gmail.com>
2025-07-16 15:34:12 +0530

    Train-Food-Delivery Project by Didin

--- train_food_delivery/settings.py
+++ train_food_delivery/settings.py
@@ -46,7 +46,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
+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    'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
@@ -120,7 +120,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 
+STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 
 # Default primary key field type
